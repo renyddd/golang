757ae2ef19b0c1cc132cc7b9a9dd52e1a03d2c8b
renyidong <renyidong@bytedance.com>
2020-11-10 10:38:08 +0800

    二分法多尝试他的集中情况 多画图写清楚变量的变化

--- leetcode/32sousuocharuweizhi/main.go
+++ leetcode/32sousuocharuweizhi/main.go
@@ -1,20 +1,13 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println("Hi")
-}
+// Learn binary search
+package bs
 
 // 如下是一个合格的二分查找，二本题的意思是要找出当数字不在时的合适索引
 func bSearch(nums []int, target int) int {
 	n := len(nums)
-	l, r := 0, n - 1
+	l, r := 0, n-1
 	var mid int
 	for l <= r {
-		mid = (r -l) >> 1 + l
+		mid = (r-l)>>1 + l
 		if nums[mid] == target {
 			return mid
 		} else if nums[mid] < target {
@@ -26,12 +19,91 @@ func bSearch(nums []int, target int) int {
 	return -1
 }
 
-func searchInsert(nums []int, target int) int {
+// bSearchFirst 有序集合存在重复数据 查找第一个等于给定值的数据
+func bSearchFirst(nums []int, target int) int {
+	n := len(nums)
+	l, r := 0, n-1
+	for l <= r {
+		m := ((r - l) >> 1) + l
+		if nums[m] > target {
+			r = m - 1
+		} else if nums[m] < target {
+			l = m + 1
+		} else if m == 0 || nums[m-1] != target {
+			return m
+		} else {
+			r = m - 1
+		}
+	}
+	return -1
+}
+
+// bSearchLast 有序集合存在重复数据 查找最后一个等于给定值的数据
+func bSearchLast(nums []int, target int) int {
+	n := len(nums)
+	l, r := 0, n-1
+	for l <= r {
+		m := ((r - l) >> 1) + l
+		if nums[m] > target {
+			r = m - 1
+		} else if nums[m] < target {
+			l = m + 1
+		} else if m == n-1 || nums[m+1] != target {
+			return m
+		} else {
+			l = m + 1
+		}
+	}
+	return -1
+}
+
+// bSearchFBigger 有序集合查找第一个大于等于给定值的数据
+func bSearchFBigger(nums []int, target int) int {
+	n := len(nums)
+	l, r := 0, n-1
+	for l <= r {
+		m := ((r - l) >> 1) + l
+		if nums[m] >= target {
+			if m == 0 || nums[m-1] < target {
+				return m
+			} else {
+				r = m - 1
+			}
+		} else {
+			l = m + 1
+		}
+	}
+	return -1
+}
+
+// bSearchLSmaller 有序集合查找最后一个小于给定值的数据
+func bSearchLSmaller(nums []int, target int) int {
 	n := len(nums)
 	l, r := 0, n - 1
+	for l <= r {
+		m := ((r - l) >> 1) + l
+		if nums[m] > target {
+			r = m - 1
+		} else if m == n-1 || nums[m+1] > target {
+			return m
+		} else {
+			l = m + 1
+		}
+	}
+	return -1
+}
+
+
+
+// leetcode 题目要求的解答
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+// 如果目标值不存在于数组中，返回它将会被按顺序插入的位置。无重复。
+func searchInsert(nums []int, target int) int {
+	n := len(nums)
+	l, r := 0, n-1
 	var mid int
 	for l <= r {
-		mid = (r -l) >> 1 + l
+		mid = (r-l)>>1 + l
 		if nums[mid] == target {
 			return mid
 		} else if nums[mid] < target {
--- leetcode/32sousuocharuweizhi/main_test.go
+++ leetcode/32sousuocharuweizhi/main_test.go
@@ -1,11 +1,11 @@
-package main
+package bs
 
 import (
 	"testing"
 )
 
 func TestSearchInsert(t *testing.T) {
-	nums := []int{1,3,5,6}
+	nums := []int{1, 3, 5, 6}
 	n := map[int]int{
 		5: 2,
 		2: 1,
@@ -20,19 +20,89 @@ func TestSearchInsert(t *testing.T) {
 }
 
 func TestBinarySearch(t *testing.T) {
-	nums := []int{1,3,5,6,7,8,10}
+	nums := []int{1, 3, 5, 6, 7, 8, 10}
 	n := map[int]int{
-		5: 2,
-		9: -1,
-		2: -1,
+		5:  2,
+		9:  -1,
+		2:  -1,
 		10: 6,
-		1: 0,
-		0: -1,
+		1:  0,
+		0:  -1,
 	}
 
 	for k, expect := range n {
 		res := bSearch(nums, k)
 		if expect != res {
+			t.Errorf("%d: expect %d, but %d", k, expect, res)
+		}
+	}
+}
+
+func TestBsearchFirst(t *testing.T) {
+	nums := []int{1, 2, 6, 6, 8, 9, 89}
+	ref := map[int]int{
+		1: 0,
+		6: 2,
+		9: 5,
+	}
+
+	for k, except := range ref {
+		res := bSearchFirst(nums, k)
+		if res != except {
+			t.Errorf("%d: except %d, but %d", k, except, res)
+		}
+	}
+}
+
+func TestBsearchLast(t *testing.T) {
+	nums := []int{1, 2, 6, 7, 7, 7, 8, 8, 9, 9}
+	ref := map[int]int{
+		2: 1,
+		6: 2,
+		7: 5,
+		8: 7,
+		9: 9,
+	}
+
+	for k, expect := range ref {
+		res := bSearchLast(nums, k)
+		if res != expect {
+			t.Errorf("%d: except %d, but %d", k, expect, res)
+		}
+	}
+}
+
+func TestBsearchFBigger(t *testing.T) {
+	nums := []int{1,2,6,7,8,9,13,14}
+	ref := map[int]int{
+		2: 1,
+		5: 2,
+		19: -1,
+		10: 6,
+	}
+
+	for k, expect := range ref {
+		res := bSearchFBigger(nums, k)
+		if res != expect {
+			t.Errorf("%d: except %d, but %d", k, expect, res)
+		}
+	}
+}
+
+func TestBsearchLSmaller(t *testing.T) {
+	nums := []int{1,2,6,7,8,9,13,14,20}
+	ref := map[int]int{
+		1: 0,
+		5: 1,
+		15: 7,
+		203: 8,
+		0: -1,
+		-3: -1,
+	}
+
+	for k, expect := range ref {
+		res := bSearchLSmaller(nums, k)
+		if res != expect {
 			t.Errorf("%d: expect %d, but %d", k, expect, res)
 		}
 	}
