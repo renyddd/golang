d00b60497ea36ac62c7c530d3f0d2126a9a8048f
renyddd <renyddd@gmail.com>
2021-02-09 16:21:53 +0800

    commit

--- leetcode/230erchasousuoshuzhongdikxiaoyuansu/main.go
+++ leetcode/230erchasousuoshuzhongdikxiaoyuansu/main.go
@@ -5,7 +5,7 @@ type TreeNode struct {
 	Left, Right *TreeNode
 }
 
-func kthSmallest(root *TreeNode, k int) int {
+func kthSmallestIterate(root *TreeNode, k int) int {
 	nums := make([]int, 0)
 	var inorderTraversal func(root *TreeNode)
 	inorderTraversal = func(root *TreeNode) {
@@ -18,6 +18,54 @@ func kthSmallest(root *TreeNode, k int) int {
 	}
 
 	inorderTraversal(root)
+
+	return nums[k-1]
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	if root == nil || k == 0 {
+		return 0
+	}
+	times := 0
+	stack := make([]*TreeNode, 0)
+	node := root
+
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			stack = append(stack, node)
+			node = node.Left
+		}
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		times++
+		if k == times {
+			return node.Val
+		}
+		node = node.Right
+	}
+	return 0
+}
+
+func kthSmallest2(root *TreeNode, k int) int {
+	if root == nil {
+		return 0
+	}
+	nums := make([]int, 0)
+	stack := make([]*TreeNode, 0)
+	node := root
+
+	for node != nil || len(stack) > 0 {
+		for node != nil {
+			stack = append(stack, node)
+			node = node.Left
+		}
+
+		node = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		nums = append(nums, node.Val)
+		node = node.Right
+	}
 
 	return nums[k-1]
 }
