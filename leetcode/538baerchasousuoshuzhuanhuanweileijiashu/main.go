9d14e3f890a83c53e397677259cb1e03ba804ce3
renyddd <renyddd@gmail.com>
2021-02-23 15:47:33 +0800

    commit

---
+++ leetcode/538baerchasousuoshuzhuanhuanweileijiashu/main.go
@@ -0,0 +1,29 @@
+package main
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+/*
+		注意题解中的求和方式
+ */
+//ref: https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+
+func convertBST(root *TreeNode) *TreeNode {
+	sum := 0
+
+	var DFS func(root *TreeNode)
+	DFS = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		DFS(root.Right)
+		sum += root.Val
+		root.Val = sum
+		DFS(root.Left)
+	}
+
+	DFS(root)
+
+	return root
+}
---
+++ leetcode/637erchashudecengpingjunzhi/main.go
@@ -0,0 +1,36 @@
+package main
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+// ref: https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+// 层序遍历练手
+
+func averageOfLevels(root *TreeNode) []float64 {
+	if root == nil {
+		return nil
+	}
+
+	curNodes := []*TreeNode{root}
+	res := make([]float64, 0)
+
+	for len(curNodes) > 0 {
+		toProcessNodes := make([]*TreeNode, 0)
+		var sum float64 = 0
+		for _, v := range curNodes {
+			sum += float64( v.Val)
+			if v.Left != nil {
+				toProcessNodes = append(toProcessNodes, v.Left)
+			}
+			if v.Right != nil {
+				toProcessNodes = append(toProcessNodes, v.Right)
+			}
+		}
+		res = append(res, sum/float64(len(curNodes)))
+		curNodes = toProcessNodes
+	}
+
+	return res
+}
